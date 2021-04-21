778b9fb8e662489fd55adc4b331ff5e0866bec40
agudo <agudonio@gmail.com>
2021-04-21 10:23:16 +0200

    NumberOfPlanTest

--- app/src/test/java/com/doubletrouble/covidrun/ExampleUnitTest.java
+++ app/src/test/java/com/doubletrouble/covidrun/ExampleUnitTest.java
@@ -1,6 +1,14 @@
 package com.doubletrouble.covidrun;
 
+import com.doubletrouble.covidrun.db.PlanDAO;
+import com.doubletrouble.covidrun.db.UsuarioDAO;
+import com.doubletrouble.covidrun.model.Plan;
+import com.doubletrouble.covidrun.model.Usuario;
+
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +21,20 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void check_newPlan(){
+        Plan testPlan = new Plan("test","test",1,1,1);
+        Usuario userTest = new Usuario("test","test","test","test","test","test","test","test",1);
+        PlanDAO IplanTest = null;
+        UsuarioDAO IuserTest;
+        List<Plan> planList = new ArrayList<Plan>();
+        planList = IplanTest.getAll();
+        int numberOfPlans=planList.size();
+        IplanTest.insertPlan(testPlan);
+        int newNumberOfPlans=planList.size();
+        assertEquals(numberOfPlans+1,newNumberOfPlans);
+
     }
 }
